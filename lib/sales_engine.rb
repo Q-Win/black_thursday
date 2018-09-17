370a0057732921643052a56f55a974deb0a12772
Quinn Krug <quinn.krug@gmail.com>
2018-09-16 18:21:23 -0600

    customer passing spec harness

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -106,8 +106,8 @@ class SalesEngine
     c_r = CustomerRepository.new
     total_customers = CSV.read(file_path_item, headers: true, header_converters: :symbol)
     total_customers.each do |customer|
-      c = Customer.new({:id => customer[:id].to_i, :first_name => customer[:first_name].to_i,
-                    :last_name => customer[:last_name].to_i,
+      c = Customer.new({:id => customer[:id].to_i, :first_name => customer[:first_name],
+                    :last_name => customer[:last_name],
                     :created_at => Time.parse(customer[:created_at]),
                     :updated_at => Time.parse(customer[:updated_at])})
       c_r.add_object(c)
